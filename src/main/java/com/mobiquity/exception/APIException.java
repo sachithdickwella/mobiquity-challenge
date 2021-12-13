06482b0d06bc00a28e29b7b884195df47b29339a
Sachith Dickwella <sachith_prasanna@live.com>
2021-12-13 22:23:57 +0800

    Adding more javadoc and README.md content

--- src/main/java/com/mobiquity/exception/APIException.java
+++ src/main/java/com/mobiquity/exception/APIException.java
@@ -3,8 +3,19 @@ package com.mobiquity.exception;
 /**
  * Implementation of {@link RuntimeException} which denotes a checked exception
  * during main functionality failure.
+ * <p>
+ * Change default implementation of {@link Exception} to {@link RuntimeException}
+ * since {@code APIException} represents exceptions during runtime. For example
+ * external input data validation cannot consider as compile time exception until
+ * the data loaded for evaluation.
+ * <p>
+ * Besides, using unchecked exception reduce the boilerplate exception handling in
+ * {@link java.util.stream.Stream} pipelines.
  *
  * @author Mobiquite
+ * @author Sachith Dickwella
+ * @version 1.0.0
+ * @see RuntimeException
  */
 public class APIException extends RuntimeException {
 
--- src/main/java/com/mobiquity/packer/Packer.java
+++ src/main/java/com/mobiquity/packer/Packer.java
@@ -12,6 +12,7 @@ import java.nio.file.Files;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
@@ -23,11 +24,12 @@ import static java.util.AbstractMap.SimpleImmutableEntry;
 import static java.util.Comparator.comparing;
 import static java.util.Comparator.naturalOrder;
 import static java.util.Comparator.reverseOrder;
+import static java.util.function.Predicate.not;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 /**
- * Class implements and expose the core functionality off the library.
+ * The class which implements and expose the core functionality off the class library.
  *
  * @author Sachith Dickwella
  * @version 1.0.0
@@ -36,8 +38,42 @@ import static java.util.stream.Collectors.toList;
 public class Packer {
 
     /**
-     * @param filePath
-     * @return
+     * Decode the item details from the file provided via the parameter {@code filePath}
+     * and calculate the output respective to each of the items.
+     * <br/>
+     * <br/>
+     * <b>Example input data:</b>
+     * <pre>{@code
+     * 81 : (1,53.38,€45) (2,88.62,€98) (3,78.48,€3) (4,72.30,€76) (5,30.18,€9) (6,46.34,€48)
+     * 8 : (1,15.3,€34)
+     * 75 : (1,85.31,€29) (2,14.55,€74) (3,3.98,€16) (4,26.24,€55) (5,63.69,€52) (6,76.25,€75) (7,60.02,€74) (8,93.18,€35) (9,89.95,€78)
+     * 56 : (1,90.72,€13) (2,33.80,€40) (3,43.15,€10) (4,37.97,€16) (5,46.81,€36) (6,48.77,€79) (7,81.80,€45) (8,19.36,€79) (9,6.76,€64)
+     * }</pre>
+     * <b>Example output string:</b>
+     * <pre>{@code
+     * 4
+     * -
+     * 2,7
+     * 8,9
+     * }</pre>
+     * <p>
+     * Core implementation to calculate the output from input reside inside this method
+     * along with the support of another two instances methods {@link #mapToItem(String)}
+     * and {@link #selectItems(Map.Entry)}.
+     * <p>
+     *
+     * @param filePath of the input data file as a {@link String}. This parameter expects
+     *                 an absolute path to a data file and user has to take care of any
+     *                 non-standard characters in the path string.
+     * @return an instance of {@link String} which represents single row result of input
+     * file by a newline. If there's no value for a particular input value, that mimics
+     * by a single {@code '-'} character in that line.
+     * @throws APIException when data validation from the input file failed or other IO
+     *                      operation failed during the same file access.
+     *                      <p>
+     *                      Throwable defined here actually nto mandatory hence it's a
+     *                      {@link RuntimeException} Keeping the declaration of throwable
+     *                      to retain the original signature, no need to handle explicitly.
      */
     @NotNull
     public static String pack(@NotNull String filePath) throws APIException {
@@ -45,9 +81,26 @@ public class Packer {
 
         try (Stream<String> lines = Files.lines(new File(filePath).toPath())) {
             return lines
-                    .filter(line -> !line.isBlank())
+                    .filter(not(String::isBlank))
+                    /*
+                     * Map each of the input file lines into {@link Map.Entry<Integer, List>},
+                     * so, process items in the downstream much easier access.
+                     */
                     .map(pack::mapToItem)
-                    .map(pack::selectItems)
+                    .map(entry -> Optional.of(pack.selectItems(entry)
+                                    .map(Item::getIndex)
+                                    /*
+                                     * This sorting is unnecessary if the output index order
+                                     * doesn't matter. Anyway, not much expensive operation
+                                     * comparing the cost it would take if, haven't order by
+                                     * the price and weight at first-hand in 'mapToItem(String)'
+                                     * method discuss later.
+                                     */
+                                    .sorted()
+                                    .map(String::valueOf)
+                                    .collect(joining(",")))
+                            .filter(not(String::isBlank))
+                            .orElse("-"))
                     .collect(joining("\n"));
         } catch (IOException ex) {
             throw new APIException(format("Error when reading '%s'", filePath), ex);
@@ -55,8 +108,27 @@ public class Packer {
     }
 
     /**
-     * @param line
-     * @return
+     * Decode the {@code line} parameter data into a {@link Map.Entry} which holds an {@link Integer}
+     * key and {@link List} of {@link Item} value which represent the weight limit and following item
+     * list respectively mapped to the {@code Map.Entry} data structure.
+     * <p>
+     * These {@link Map.Entry}s never going to become full-fledged {@link Map} hence they are treated
+     * in a {@link Stream} pipeline and mid-stream operators extract only the required elements from
+     * the datasets.
+     * <br/>
+     * <br/>
+     * <b>Behavior (For single line):</b>
+     * <br/>
+     * <ol>
+     * <li>Initially parameter {@code line }get split by {@code ':'} character to extract weight limit at
+     * the beginning of each line, trim it and set as the return {@link Map.Entry} key.</li>
+     * <li></li>
+     * </ol>
+     *
+     *
+     * @param line extracted from the input file which contains the weight limit and items details with
+     *             miscellaneous characters that does not participate in the process.
+     * @return an instance of {@link Map.Entry} with decoded line information from the input file lines.
      */
     @NotNull
     private Map.Entry<Integer, List<Item>> mapToItem(@NotNull String line) {
@@ -81,16 +153,17 @@ public class Packer {
                 throw new APIException("Item list and weight limit not defined properly");
             }
         } catch (ArrayIndexOutOfBoundsException | NumberFormatException ex) {
-            throw new APIException(format("Item data '%s' invalid", line), ex);
+            throw new APIException(format("Item list '%s' invalid", line), ex);
         }
     }
 
     /**
-     * @param entry
+     * @param entry an instance of {@link Map.Entry} (key-value) pair, weight limit
+     *              as the key and {@link List} of {@link Item}s as the value.
      * @return
      */
     @NotNull
-    private String selectItems(@NotNull Map.Entry<Integer, List<Item>> entry) {
+    private Stream<Item> selectItems(@NotNull Map.Entry<Integer, List<Item>> entry) {
         var selected = new LinkedList<Item>();
         var count = new AtomicReference<>(0D);
 
@@ -101,10 +174,6 @@ public class Packer {
             }
         });
 
-        return selected.isEmpty() ? "-" : selected.stream()
-                .map(Item::getIndex)
-                .sorted()
-                .map(String::valueOf)
-                .collect(joining(","));
+        return selected.stream();
     }
 }
